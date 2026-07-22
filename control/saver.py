ca610f06ca64e84c9fb17b0caafaa99d7a79ae4e
17isgreaterthan18 <wer1928@rit.edu>
2026-07-22 13:59:18 -0400

    saver now generic

--- control/saver.py
+++ control/saver.py
@@ -6,40 +6,38 @@ import sched
 
 from util.const import (
     SAVE_INTERVAL_S,
-    CSV_HEADER,
 )
 from util.data import (
-    SamplerData
+    Data
 )
 
 
 class SaveRunner(Thread):
     def __init__(self, 
-        filepath : str, 
+        filepath : str,
+        memo : str,
         data_queue : SimpleQueue, 
         stop_event : Event,
-        payload_header : str
     ):
         super().__init__()
-
         self.filepath = filepath
         self.queue = data_queue
         self.stop_event = stop_event
-        self.payload_header = payload_header
+        self.memo = memo
+        self.first = True
 
     def save(self, sc : sched.scheduler | None):
-        d : SamplerData
+        d : Data
         with open(self.filepath, 'a') as f:
             while not self.queue.empty():
                 try:
                     d = self.queue.get_nowait()
                 except Empty:
                     break
-                f.write(','.join( [str(x) for x in (
-                    d.timestamp, d.delta_time, d.voltage, d.current, d.power,
-                    d.e_field, d.curr_density, d.power_density, d.temperature,
-                    *d.payload
-                )] ) + '\n')
+                if self.first:
+                    f.write(d.header + '\n')
+                    self.first = False
+                f.write(','.join( [str(x) for x in d] ) + '\n')
         
         if not self.stop_event.is_set() and sc != None:
             sc.enter(SAVE_INTERVAL_S, 1, self.save, (sc, ))
@@ -47,7 +45,7 @@ class SaveRunner(Thread):
     def run(self):
         print('Saving thread started')
         with open(self.filepath, 'w') as f:
-            f.write(CSV_HEADER + ',' + self.payload_header + '\n')
+            f.write('# ' + self.memo + '\n')
 
         sc = sched.scheduler(time.monotonic, time.sleep)
         sc.enter(SAVE_INTERVAL_S, 1, self.save, (sc, ))
--- util/data.py
+++ util/data.py
@@ -1,18 +1,78 @@
 from dataclasses import dataclass
-from typing import NamedTuple
+from typing import NamedTuple, ClassVar
+from abc import ABC, abstractmethod
+
+from control.multimeter import (
+    Mode
+)
+
+
+FLASH_CSV_HEADER = 'Timestamp,Time,Volts,Amps,|S|(VA),E,J,P,T'
+AC_PAYLOAD_CSV_HEADER = 'S (VA),freq (Hz)'
+DC_PAYLOAD_CSV_HEADER = ''
 
 @dataclass
-class SupplyData:
+class Payload:
+    def __iter__(self):
+        return iter(tuple())
+    @property
+    def header(self) -> str:
+        return ''
+
+@dataclass
+class ACPayload(Payload):
+    complex_power: complex
+    frequency : float
+
+    def __iter__(self):
+        return iter((self.complex_power, self.frequency))
+    @property
+    def header(self) -> str:
+        return AC_PAYLOAD_CSV_HEADER
+
+
+@dataclass
+class Data(ABC):
+    time : float
+
+    def __iter__(self):
+        return iter((self.time))
+    @abstractmethod
+    @property
+    def header(self) -> str:
+        pass
+
+@dataclass
+class MultimeterData(Data):
+    timestamp : float
+    time : float
+    value : float
+    mode : Mode
+
+    def __iter__(self):
+        return iter((self.timestamp, self.time, self.value))
+    @property
+    def header(self):
+        return 'timestamp,time,' + str(self.mode.name)
+
+@dataclass
+class SupplyData(Data):
     voltage : float
     current : float
     power : float
     timestamp : float # provided by datetime.datetime.now().timestamp()
-    monotonic_time : float # provided by time.monotonic()
-    payload : NamedTuple = tuple()
+    time : float # provided by time.monotonic()
+    payload : Payload = Payload()
     temperature : float | None = None # TODO seperate temperature collection?
 
+    def __iter__(self):
+        return iter((self.timestamp, self.time, self.voltage, self.current, self.power, self.temperature, *self.payload))
+    @property
+    def header(self) -> str:
+        return '' # TODO maybe
+
 @dataclass
-class SamplerData:
+class SamplerData(Data):
     voltage : float
     current : float
     power : float
@@ -20,14 +80,23 @@ class SamplerData:
     curr_density : float
     power_density : float
     timestamp : float
-    delta_time : float
+    time : float
     temperature : float
-    payload : NamedTuple = tuple()
+    payload : Payload = Payload()
 
-class ACPayload(NamedTuple):
-    complex_power: complex
-    frequency : float
-
-AC_PAYLOAD_CSV_HEADER = 'S (VA),freq (Hz)'
-
-DC_PAYLOAD_CSV_HEADER = ''
+    def __iter__(self):
+        return iter(
+            self.timestamp,
+            self.time,
+            self.voltage,
+            self.current,
+            self.power,
+            self.e_field,
+            self.curr_density,
+            self.power_density,
+            self.temperature,
+            *self.payload
+        )
+    @property
+    def header(self):
+        return FLASH_CSV_HEADER + ',' + self.payload.header
